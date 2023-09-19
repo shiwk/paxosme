dafa1c4e0aad7995eb9ab653e4873e0feaaccd5a
shiwk <shiwktju@gmail.com>
2023-09-20 00:14:40 +0800

    segment_store cases

--- unittest/storage/segment_test_cases.cpp
+++ unittest/storage/segment_test_cases.cpp
@@ -58,17 +58,24 @@ TEST_F(TestSegmentStoreTests, SegmentStoreInitEmptyDirPath)
     EXPECT_FALSE(initResult);
 }
 
-TEST_F(TestSegmentStoreTests, InitWithSegmentAndReadWrite)
+TEST_F(TestSegmentStoreTests, InitWithSegmentAndReadWriteInFirstSegment)
 {
     auto segmentStore = ShortLife::CreateInstance<LogSegmentStore>();
 
-    paxosme::LogStorage::LogStorageOptions logStorageOptions = {DirPath, 1024, SizeString::HexStringSize<instance_id_t>()};
+    size_t segmentMaxSize = 1024;
+    paxosme::LogStorage::LogStorageOptions logStorageOptions = {DirPath, segmentMaxSize, SizeString::HexStringSize<instance_id_t>()};
 
     bool initResult = segmentStore->Init(logStorageOptions);
     EXPECT_TRUE(initResult);
 
-    for (instance_id_t i = 1; i <= 2; ++i)
-    {
+    std::vector<SegmentIndex> segmentIndexs;
+
+    off_t offset = 0;
+    instance_id_t i = 1;
+    SEGMENT_ID segmentId = 0;
+
+    do {
+        ASSERT_EQ(0, segmentId);
         const std::string &value = "test-value" + std::to_string(i);
 
         SegmentIndex segment_index;
@@ -77,6 +84,7 @@ TEST_F(TestSegmentStoreTests, InitWithSegmentAndReadWrite)
         LOG(INFO) << "key:" << key << ", len:" << key.size();
         bool appendResult = segmentStore->Append(key, value, segment_index);
         LOG(INFO) << "segment_index:" << segment_index;
+        segmentIndexs.push_back(segment_index);
         EXPECT_TRUE(appendResult);
         EXPECT_FALSE(segment_index.empty());
 
@@ -84,8 +92,26 @@ TEST_F(TestSegmentStoreTests, InitWithSegmentAndReadWrite)
         bool readResult = segmentStore->Read(segment_index, key, readValue);
         EXPECT_TRUE(readResult) << "key: "<< key;
         EXPECT_EQ(value, readValue);
+        
+        CHECKSUM checkSum;
+        LogSegmentStore::ParseSegmentIndex(segment_index, segmentId, offset, checkSum);
+        ++i;
+    }while (offset + segmentId * segmentMaxSize < segmentMaxSize);
+
+    for (i = segmentIndexs.size() - 1; i >= 1; --i)
+    {
+        const std::string &value = "test-value" + std::to_string(i);
+        std::string key = SizeString::ToHexString(i);
+        std::string readValue;
+        bool readResult = segmentStore->Read(segmentIndexs[i - 1], key, readValue);
+        ASSERT_TRUE(readResult) << "key: "<< key;
+        ASSERT_EQ(value, readValue);
     }
-    // todo I: more cases
+}
+
+TEST_F(TestSegmentStoreTests, InitWithSegmentAndReadWriteInMultiSegments)
+{
+    // todo I: add test case for multi segments
 }
 
 TEST_F(TestSegmentStoreTests, InitWithSegmentExistSegment)
